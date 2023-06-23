5a30ef9715c259b8c7547072c11d098047642ae6
Mario Lazaro <mario-msl@hotmail.com>
2023-06-23 18:56:49 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+import javax.swing.SpringLayout;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parametro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o Segundo Parametro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }
+        catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+    
+}
